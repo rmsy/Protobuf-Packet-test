d720c4d7861ad0547c06e08d3acce3cc705c1d24
Isaac Moore <rmsy@me.com>
2013-03-16 18:28:19 -0500

    Update to use ProjectAres/Protocol

--- src/main/java/com/github/rmsy/Main.java
+++ src/main/java/com/github/rmsy/Main.java
@@ -1,7 +1,7 @@
 package com.github.rmsy;
 
 import com.github.rmsy.handlers.TestMessageHandler;
-import com.github.rmsy.protocol.TestProto;
+import com.github.rmsy.protocol.PATestProto;
 import net.anxuiz.protobuf.packet.MessageHandlerRegistry;
 import net.anxuiz.protobuf.packet.PacketManager;
 import net.anxuiz.protobuf.packet.base.SimpleMessageHandlerRegistry;
@@ -17,14 +17,14 @@ public class Main {
     private final MessageHandlerRegistry registry;
 
     private Main(@Nonnull MessageHandlerRegistry registry) {
-        registry.register(TestProto.ExtendingMessage.getDescriptor(), new TestMessageHandler());
+        registry.register(PATestProto.PlayerJoin.getDescriptor(), new TestMessageHandler());
         this.registry = registry;
     }
 
     public static void main(String[] args) {
         MessageHandlerRegistry registry = new SimpleMessageHandlerRegistry();
         new Main(registry);
-        PacketManager<TestProto.DefaultMessage> manager = new SimplePacketManager<>(TestProto.DefaultMessage.newBuilder().setExtension(TestProto.ExtendingMessage.msg, TestProto.OtherMessage.newBuilder().build()).build());
+        PacketManager<PATestProto.Packet> manager = new SimplePacketManager<>(PATestProto.Packet.newBuilder().setExtension(PATestProto.PlayerJoin.playerJoin, PATestProto.PlayerJoin.newBuilder().setName("iamramsey").setDisplayName("Isaac Moore").build()).build());
         System.out.println(manager.parse(registry));
     }
 }
